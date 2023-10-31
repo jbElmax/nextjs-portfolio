bed758d0c995bf278ccd425857e4815719959b98
Jan Elnas <jan.elnas@gmail.com>
2023-10-31 13:54:35 +0800

    initial commit

---
+++ app/work-experience/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WorkExperiencePage = () => {
+  return (
+    <div className='p-1 text-black'>WorkExperiencePage</div>
+  )
+}
+
+export default WorkExperiencePage;
